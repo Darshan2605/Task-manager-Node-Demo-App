bf4655988f98ddc874865a0a4231e8e9091f0a97
Darshan Londhe <116908065+Darshan2605@users.noreply.github.com>
2025-02-19 22:45:26 +0530

    update dependencies

--- src/app.js
+++ src/app.js
@@ -15,6 +15,9 @@ app.use('/api/tasks', taskRoutes);
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
 
 // Start server
 const PORT = process.env.PORT || 3000;
